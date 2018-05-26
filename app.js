9fd40d5fcff73ebdc83c9e0fc44c063bf4a2f941
Khang <15520345@gm.uit.edu.vn>
2018-05-26 18:11:41 +0700

    Second commit

--- app.js
+++ app.js
@@ -5,14 +5,29 @@ const routesOrders = require("./api/routes/orders");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
+// morgan is a static plugin on command 
 app.use(morgan('dev'));
+// body-parser help to parse data from POST method, extended means using lot of data or not
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
+// allow Cors : Cross Origin Resource Sharing
+app.use((req,res,next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authoriztion");
+	if (req.method === "OPTIONS")
+	{
+		res.header("Access-Control-Allow-Methods", "PUT, POST, GET, PATCH, DELETE");
+		return res.status(200).json();
+	}
+	next();
+});
 
 app.use('/products', routesProduct);
 app.use('/orders', routesOrders);
 
-
+// if these method won't use route above, then it will come here to identify error
+// next means forward error object to next function
+// next function help to display error.message
 app.use((req,res,next) => {
 	const error = new Error("Not found");
 	error.status = 404;
